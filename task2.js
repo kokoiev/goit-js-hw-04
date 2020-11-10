49492d53b4f645acc44b18c8c89f7f12cd8f31fd
sqralan <sqralan@ukr.net>
2020-11-10 13:52:44 +0200

    second task finished

--- task2.js
+++ task2.js
@@ -1,33 +1,30 @@
 const inventory = {
   items: ['Knife', 'Gas mask'],
   add(itemName) {
-      console.log(`Adding ${itemName} to inventory`);
-      this.items.push(itemName);
-      
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
   },
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
-for (const item of this.items ) {
+
     this.items = this.items.filter(item => item !== itemName);
-}
-    
-    },
-  invokeInventoryAction(itemName, action) {
-      console.log(`Invoking action on ${itemName}`);
-  const bindAction = action.bind(this)    
-  bindAction(itemName);
-}
+  },
+};
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
 };
 
 
-
-inventory.invokeInventoryAction('Medkit', inventory.add);
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-inventory.invokeInventoryAction('Gas mask', inventory.remove);
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
